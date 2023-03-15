aa8b4ecc130b7f4cab60242a33710273c5a71040
Emmanuel <mziyaemmanuel@gmail.com>
2023-03-15 01:10:24 +0000

    avg temp

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- Import in hbtn_0c_0 database this table dump
+-- Displays cities average temperature
+SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+
